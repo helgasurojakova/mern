c424ac6ae18e680c51ed3ac26fd8d64daaba8fe5
helgasurojakova <olyasuroyacova@mail.ru>
2022-02-17 23:06:44 +0300

    basic routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import {useRoutes} from "./routes";
+import {BrowserRouter} from "react-router-dom";
 
 function App() {
+    const routes = useRoutes(false)
   return (
-    <div>
-      <h1>WORKS</h1>
-    </div>
+    <BrowserRouter>
+      <div>
+          {routes}
+      </div>
+    </BrowserRouter>
   );
 }
 

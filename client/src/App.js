7f756eeebc7ed9b4b329765b762de1b022529a26
helgasurojakova <olyasuroyacova@mail.ru>
2022-03-09 16:15:29 +0300

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {useRoutes} from "./routes"
 import {BrowserRouter} from "react-router-dom"
 import {useAuth} from "./hooks/auth.hook"
 import {AuthContext} from "./context/AuthContext"
+import Navbar from "./components/Navbar";
 
 function App() {
     const {token, login, logout, userId} = useAuth()
@@ -13,6 +14,7 @@ function App() {
           token, login, logout, userId, isAuthenticated
       }}>
           <BrowserRouter>
+              {isAuthenticated && <Navbar/>}
               <div>
                   {routes}
               </div>
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,57 @@
+import React, {useContext} from 'react'
+import {AuthContext} from "../context/AuthContext";
+import {
+    Box, Button,
+    Flex,
+    HStack,
+    Link,
+    useColorModeValue
+} from "@chakra-ui/react";
+
+const Links = ['Create', 'Links'];
+const NavLink = ({ children }) => (
+    <Link
+        px={2}
+        py={1}
+        rounded={'md'}
+        _hover={{
+            textDecoration: 'none',
+            bg: useColorModeValue('gray.200', 'gray.700'),
+        }}
+        href={children}>
+        {children}
+    </Link>
+);
+
+const Navbar = () => {
+    const auth = useContext(AuthContext)
+    const logoutHandler = (e) => {
+        e.preventDefault()
+        auth.logout()
+        window.location.href = '/'
+    }
+    return (
+        <>
+            <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+                <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+                    <HStack spacing={8} alignItems={'center'}>
+                        <Box>Logo</Box>
+                        <HStack
+                            as={'nav'}
+                            spacing={4}
+                            display={{ base: 'none', md: 'flex' }}>
+                            {Links.map((link) => (
+                                <NavLink key={link}>{link}</NavLink>
+                            ))}
+                        </HStack>
+                    </HStack>
+                    <Flex alignItems={'center'}>
+                        <Button onClick={logoutHandler}>Logout</Button>
+                    </Flex>
+                </Flex>
+            </Box>
+        </>
+    );
+};
+
+export default Navbar;

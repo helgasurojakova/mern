5d4a89cc249018c582255f5ed8882143becb39c6
helgasurojakova <91551597+helgasurojakova@users.noreply.github.com>
2022-04-19 17:21:18 +0200

    redirect routes, fix

--- App.js
+++ App.js
@@ -8,14 +8,13 @@ app.use(express.json({extended: true}))
 
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/link', require('./routes/link.routes'))
+app.use('/t', require('./routes/redirect.routes'))
 
 const PORT = config.get('port') || 5000
 
 async function start() {
     try {
-        await mongoose.connect(config.get('mongoUri'), {
-            useNewUrlParser: true,
-        })
+        await mongoose.connect(config.get('mongoUri'), {})
         app.listen(PORT, () => console.log(`App has been started on port ${PORT}`))
     } catch (e) {
         console.log('Server Error', e.message)
